e9fb25e3804addc92d033bdbc4188795675e9c5b
konrad.kraemer <konrad.kraemer@kit.edu>
2022-05-28 14:54:02 +0200

    added pointer interface

---
+++ lib/sourceCpp-x86_64-pc-linux-gnu-1.0.7/sourcecpp_3c642a1ea600/file3c641446cf5f.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('/home/konrad/Documents/0Uni/programming/UseR_presentation/lib/sourceCpp-x86_64-pc-linux-gnu-1.0.7/sourcecpp_3c642a1ea600/sourceCpp_2.so')
+
+getXPtr <- Rcpp:::sourceCppFunction(function() {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_getXPtr')
+
+rm(`.sourceCpp_1_DLLInfo`)
