edcd1429e3b7e936c6b6ef9db1ed8ef67ed76816
konrad.kraemer <konrad.kraemer@kit.edu>
2022-06-21 15:50:02 +0200

    update r interfcae in example

---
+++ lib/sourceCpp-x86_64-pc-linux-gnu-1.0.8.3/sourcecpp_acb87732ce03/fileacb823ced76a.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_3_DLLInfo` <- dyn.load('/home/konrad/Documents/0Uni/programming/UseR_presentation/lib/sourceCpp-x86_64-pc-linux-gnu-1.0.8.3/sourcecpp_acb87732ce03/sourceCpp_4.so')
+
+getXPtr <- Rcpp:::sourceCppFunction(function() {}, FALSE, `.sourceCpp_3_DLLInfo`, 'sourceCpp_3_getXPtr')
+
+rm(`.sourceCpp_3_DLLInfo`)
